18b6f98b07600fb4f02c0c360bd44605ce879c43
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-05 14:36:26 -0300

    #US4 - NewFeature-clinicaDataRequest - finalizacao

--- src/main/java/com/tuyo/clinicaapi/controllers/ClinicaDataController.java
+++ src/main/java/com/tuyo/clinicaapi/controllers/ClinicaDataController.java
@@ -5,7 +5,10 @@ import com.tuyo.clinicaapi.model.ClinicaData;
 import com.tuyo.clinicaapi.model.Paciente;
 import com.tuyo.clinicaapi.repository.ClinicaDataRepository;
 import com.tuyo.clinicaapi.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,8 +18,9 @@ public class ClinicaDataController {
            private ClinicaDataRepository clinicosRepo;                                       // Busca(fetch) pelo paciente
            private PacienteRepository pacienteRepo;                                         // Injetando o PacienteRepository e também o ClinicaRepository
 
-            ClinicaDataController(ClinicaDataRepository clinicaRepo, PacienteRepository pacienteRepo) {      // Criado o Construtor. Terá como passagem de parâmetro ClinicaDataRepository e PacienteRepository
-                    this.clinicosRepo = clinicaRepo;
+            @Autowired
+            ClinicaDataController(ClinicaDataRepository clinicosRepo, PacienteRepository pacienteRepo) {      // Criado o Construtor. Terá como passagem de parâmetro ClinicaDataRepository e PacienteRepository
+                    this.clinicosRepo = clinicosRepo;
                     this.pacienteRepo = pacienteRepo;
             }
                                                                                    // ******  US_3 - Como uma pessoa atendente de consultório médico, eu quero acessar e salvar dados clínicos por paciente individualmente ******
@@ -31,5 +35,12 @@ public class ClinicaDataController {
             clinicaData.setPaciente(paciente);                                                  //recuperando o paciente do database
             return clinicosRepo.save(clinicaData);                                                                        // Iremos criar um request personalizado chamado ClinicaDataRequest.
                                                                                             // Isto é um bean que estamos criando  para controlar as batidas
-    }                                                                                      // @RequestBody = é necessário marcar para que o Spring não serialize a request automaticamente.
-}                                                                                          // Para ClinicaDataRequest, precisamos criar uma ClinicaData Bean.
+    }                                                                                     // @RequestBody = é necessário marcar para que o Spring não serialize a request automaticamente.
+                                                                                         // Para ClinicaDataRequest, precisamos criar uma ClinicaData Bean.
+
+/*    @RequestMapping(value = "/dadosclinicos/{pacienteId}/{componenteNome}", method = RequestMethod.GET)
+    public List<ClinicaData> getClinicaData(@PathVariable("pacienteId") int pacienteId,
+                                             @PathVariable("componenteNome") String componenteNome) {
+        return clinicosRepo.findByPacienteIdAndComponenteNomeOrderByMeasuredDateTime(pacienteId, componenteNome);
+    }*/
+}
--- src/main/java/com/tuyo/clinicaapi/repository/ClinicaDataRepository.java
+++ src/main/java/com/tuyo/clinicaapi/repository/ClinicaDataRepository.java
@@ -3,5 +3,9 @@ package com.tuyo.clinicaapi.repository;
 import com.tuyo.clinicaapi.model.ClinicaData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClinicaDataRepository extends JpaRepository<ClinicaData, Integer> {
+
+   /* List<ClinicaData> findByPacienteIdAndComponenteNomeOrderByMeasuredDateTime(int pacienteId, String componenteNome);*/
 }
